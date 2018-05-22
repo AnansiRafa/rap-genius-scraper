a3ccb2e15e089765f8d2e2357213497c9042814a
Anthony Cook <antrafcook@gmail.com>
2018-05-22 12:20:46 -0400

    added index page for songs, need to add show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'songs#random'
   get 'songs/:id', to: 'songs#show'
+  get '/index', to: 'songs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
